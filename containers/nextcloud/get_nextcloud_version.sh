db9ec0af418b84934a6d69c90fb069cd12e3a46b
Johannes kingma <theking2@king.ma>
2025-05-07 17:37:30 +0200

    add common questionaire
    For support requests

--- containers/nextcloud/get_nextcloud_version.sh
+++ containers/nextcloud/get_nextcloud_version.sh
@@ -2,13 +2,22 @@
 
 reverse_proxy_version=$(docker container exec nginx-proxy nginx -v 2>&1 | head -n 1)
 
-echo "* Nextcloud Server version _(e.g., 29.x.x)_:  
-  - \`$(docker container exec nextcloud php /var/www/html/occ --version)\`  
-* Operating system and version _(e.g., Ubuntu 24.04)_:  
-  - \`$(docker container exec nextcloud uname -v)\`  
-* Web server and version _(e.g., Apache 2.4.25)_:  
-  - \`$(docker container exec nextcloud apachectl -v | head -n 1)\`  
-* Reverse proxy and version _(e.g., nginx 1.27.2)_:  
-  - \`$reverse_proxy_version\`  
-* PHP version _(e.g., 8.3)_:  
-  - \`$(docker container exec nextcloud php --version | head -n 1)\`"
+echo "* Nextcloud Server version:
+  - \`$(docker container exec nextcloud php /var/www/html/occ --version)\`
+* Operating system and version:
+  - \`$(docker container exec nextcloud uname -v)\`
+* Web server and version:
+  - \`$(docker container exec nextcloud apachectl -v | head -n 1)\`
+* Reverse proxy and version:
+  - \`$reverse_proxy_version\`
+* PHP version:
+  - \`$(docker container exec nextcloud php --version | head -n 1)\`
+* Is this the first time you've seen this error? (Yes / No):
+  - \`Yes\`
+* When did this problem seem to first start?
+  - \`direct after installation or reboot\`
+* Installation method _(e.g. AlO, NCP, Bare Metal/Archive, etc.)_
+  - \`OMV Container\`
+* Are you using CloudfIare, mod_security, or similar? _(Yes / No)_
+  - \`No\`
+"
